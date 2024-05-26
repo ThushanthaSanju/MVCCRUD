677772902688ebee649abb714d48b06d0d72ad99
ThushanthaSanju <thushanthasanju8@gmail.com>
2024-05-26 21:54:32 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCRUD.Data;
+using MVCCRUD.Models;
+using MVCCRUD.Models.Domain;
+
+namespace MVCCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly CrudDBContext crudDBContext;
+
+        public EmployeeController(CrudDBContext crudDBContext)
+        {
+            this.crudDBContext = crudDBContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest) 
+        {
+           var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+            
+           await crudDBContext.Employees.AddAsync(employee);
+            await crudDBContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+            }
+        
+    }
+}
