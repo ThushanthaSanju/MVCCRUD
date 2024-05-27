9702b4b727571e74e5542c7262615d9d61d613f1
ThushanthaSanju <thushanthasanju8@gmail.com>
2024-05-27 11:04:21 +0530

    second

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVCCRUD.Data;
 using MVCCRUD.Models;
 using MVCCRUD.Models.Domain;
@@ -13,12 +14,20 @@ namespace MVCCRUD.Controllers
         {
             this.crudDBContext = crudDBContext;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await crudDBContext.Employees.ToListAsync();
+          return View(employees);
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
-
+      
         [HttpPost]
         public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest) 
         {
@@ -34,8 +43,64 @@ namespace MVCCRUD.Controllers
             
            await crudDBContext.Employees.AddAsync(employee);
             await crudDBContext.SaveChangesAsync();
-            return RedirectToAction("Add");
+            return RedirectToAction("Index");
             }
-        
+
+        [HttpGet]
+        public async  Task<IActionResult> View(Guid id)
+        {
+            var employee = await crudDBContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (employee != null)
+            {
+                var ViewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBirth = employee.DateOfBirth
+                };
+                return await Task.Run(()=> View("View",ViewModel));
+            }
+           
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel updateEmployeeRequest)
+        {
+            var employee = await crudDBContext.Employees.FirstOrDefaultAsync(e => e.Id == updateEmployeeRequest.Id);
+
+            if (employee != null)
+            {
+                employee.Name = updateEmployeeRequest.Name;
+                employee.Email = updateEmployeeRequest.Email;
+                employee.Salary = updateEmployeeRequest.Salary;
+                employee.Department = updateEmployeeRequest.Department;
+                employee.DateOfBirth = updateEmployeeRequest.DateOfBirth;
+
+                crudDBContext.Employees.Update(employee);
+                await crudDBContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel updateEmployeeRequest)
+        {
+            var employee = await crudDBContext.Employees.FindAsync(updateEmployeeRequest.Id);
+
+            if (employee != null)
+            {
+                crudDBContext.Employees.Remove(employee);
+                await crudDBContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
     }
 }
